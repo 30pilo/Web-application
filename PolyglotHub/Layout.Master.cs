c1068bd899f78aa486f8f5cf53f8cd0e1b450185
sunnic <nicholassun260@gmail.com>
2023-07-12 00:29:28 +0800

    Added Response Redirect to footer admin functions

--- PolyglotHub/Layout.Master.cs
+++ PolyglotHub/Layout.Master.cs
@@ -23,5 +23,43 @@ namespace PolyglotHub
         {
             Response.Redirect("AdminGrammarManagement.aspx");
         }
+
+        protected void LinkButton7_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminQuestionManagement.aspx");
+        }
+
+        protected void LinkButton8_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminVocabularyManagement.aspx");
+        }
+
+        protected void LinkButton9_Click(object sender, EventArgs e) // Forum Management Page
+        {
+            Response.Redirect("#");
+        }
+
+        protected void LinkButton10_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminMemberManagement.aspx");
+        }
+
+        protected void LinkButton11_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Test11");
+            Response.Redirect("AdminTestManagement.aspx");
+        }
+
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Test4");
+            Response.Redirect("LoginPage.aspx");
+        }
+
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Test3");
+            Response.Redirect("SignUpPage.aspx");
+        }
     }
 }
