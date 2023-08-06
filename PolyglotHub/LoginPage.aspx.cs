aaf224e08a0e2bb2164f97b732a3577444ff6b1a
sunnic <nicholassun260@gmail.com>
2023-08-07 00:00:33 +0800

    Fix form validation for login and signup

--- PolyglotHub/LoginPage.aspx.cs
+++ PolyglotHub/LoginPage.aspx.cs
@@ -23,34 +23,42 @@ namespace PolyglotHub
             
             try
             {
-                SqlConnection con = new SqlConnection(strcon);
-                if(con.State == ConnectionState.Closed)
-                {
-                    con.Open();
-                }
-
-                string q1 = "SELECT * FROM MemberTable WHERE Username='"+TextBox1.Text.Trim()+"' AND Password='"+TextBox2.Text.Trim()+"'";
-                SqlCommand cmd1 = new SqlCommand(q1, con);
-                // Connected Architecture -> DR will still be connected to DB to the data table
-                SqlDataReader dr = cmd1.ExecuteReader();
-                if(dr.HasRows) // Give true or false depends on record coming in or not
-                {
-                    while(dr.Read())
-                    {
-                        Response.Write("<script>alert('Login Success!'); window.location='Default.aspx';</script>"); //  if all condition are true
-                        Session["MemberID"] = dr.GetValue(0).ToString();
-                        Session["username"] = dr.GetValue(1).ToString();
-                        Session["firstname"] = dr.GetValue(3).ToString();
-                        Session["lastname"] = dr.GetValue(4).ToString();
-                        Session["role"] = "Member";
-                        Session["status"] = dr.GetValue(7).ToString();
-                    }
-                } else
+                if(TextBox1.Text.Equals("") || TextBox2.Text.Equals(""))
                 {
                     errLabel1.Visible = true;
-                    errLabel1.Text = "Username/Password Not Found!";
-                }
-                
+                    errLabel1.Text = "Username/Password must be filled !";
+                } else
+                {
+                    errLabel1.Visible = false;
+                    SqlConnection con = new SqlConnection(strcon);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    string q1 = "SELECT * FROM MemberTable WHERE Username='" + TextBox1.Text.Trim() + "' AND Password='" + TextBox2.Text.Trim() + "'";
+                    SqlCommand cmd1 = new SqlCommand(q1, con);
+                    // Connected Architecture -> DR will still be connected to DB to the data table
+                    SqlDataReader dr = cmd1.ExecuteReader();
+                    if (dr.HasRows) // Give true or false depends on record coming in or not
+                    {
+                        while (dr.Read())
+                        {
+                            Response.Write("<script>alert('Login Success!'); window.location='Default.aspx';</script>"); //  if all condition are true
+                            Session["MemberID"] = dr.GetValue(0).ToString();
+                            Session["username"] = dr.GetValue(1).ToString();
+                            Session["firstname"] = dr.GetValue(3).ToString();
+                            Session["lastname"] = dr.GetValue(4).ToString();
+                            Session["role"] = "Member";
+                            Session["status"] = dr.GetValue(7).ToString();
+                        }
+                    }
+                    else
+                    {
+                        errLabel1.Visible = true;
+                        errLabel1.Text = "Username/Password Not Found!";
+                    }
+                }    
             } catch (Exception ex)
             {
                 errLabel1.Visible = true;
--- PolyglotHub/SignUpPage.aspx.cs
+++ PolyglotHub/SignUpPage.aspx.cs
@@ -21,55 +21,66 @@ namespace PolyglotHub
         // Submit/ Signup Button Clicked
         protected void Button1_Click(Object sender, EventArgs e)
         {
-            try
+            if(TextBox3.Text.Equals("") || TextBox4.Text.Equals("") || TextBox1.Text.Equals("") || TextBox2.Text.Equals("") || TextBox6.Text.Equals(""))
             {
-                SqlConnection con = new SqlConnection(strcon);
-                if (con.State == ConnectionState.Closed) //  If connection is closed, Open it
-                {
-                    con.Open();
-                }
-
-                string q1 = "SELECT COUNT(*) FROM MemberTable WHERE Username = '" + TextBox1.Text.Trim() + "'"; // Check for Username Status
-                SqlCommand cmd1 = new SqlCommand(q1, con);
-                int check = Convert.ToInt32(cmd1.ExecuteScalar().ToString());
-
-                if(check > 0)
-                {
-                    errLabel1.Visible = true;
-                    errLabel1.Text = "Username has been Used!";
-                } else
-                {
-                    if(DropDownList1.SelectedItem.Value == "select")
-                    {
-                        errLabel3.Visible = true;
-                        errLabel3.Text = "Select your country!";
-                    } else
-                    {
-                        string q2 = "INSERT INTO MemberTable (Username,Password,FirstName,LastName,Country,DateJoined,LoginStatus)" +
-                            "VALUES (@Username,@Password,@FirstName,@LastName,@Country,@DateJoined,@LoginStatus)";
-                        SqlCommand cmd2 = new SqlCommand(q2, con);
-                        DateTime today = DateTime.Now.Date;
-
-                        cmd2.Parameters.AddWithValue("@FirstName", TextBox3.Text.Trim());
-                        cmd2.Parameters.AddWithValue("@LastName", TextBox4.Text.Trim());
-                        cmd2.Parameters.AddWithValue("@Country", DropDownList1.SelectedItem.Value);
-                        cmd2.Parameters.AddWithValue("@Username", TextBox1.Text.Trim());
-                        cmd2.Parameters.AddWithValue("@Password", TextBox2.Text.Trim());
-                        cmd2.Parameters.AddWithValue("@DateJoined", today.ToString("yyyy-MM-dd"));
-                        cmd2.Parameters.AddWithValue("@LoginStatus", "Pending");
-
-                        cmd2.ExecuteNonQuery();
-                        Response.Write("<script>alert('Sign Up Success!'); window.location='LoginPage.aspx';</script>");
-                    }
-                }
-                con.Close();
-                
-            }
-            catch (Exception ex)
-            {
+                errLabel2.Text = "ALL Field must be filled !";
                 errLabel2.Visible = true;
-                errLabel2.Text = "Registration Failed!" + ex.ToString(); // need to check
+            } else
+            {
+                errLabel2.Visible = false;
+                try
+                {
+                    SqlConnection con = new SqlConnection(strcon);
+                    if (con.State == ConnectionState.Closed) //  If connection is closed, Open it
+                    {
+                        con.Open();
+                    }
+
+                    string q1 = "SELECT COUNT(*) FROM MemberTable WHERE Username = '" + TextBox1.Text.Trim() + "'"; // Check for Username Status
+                    SqlCommand cmd1 = new SqlCommand(q1, con);
+                    int check = Convert.ToInt32(cmd1.ExecuteScalar().ToString());
+
+                    if (check > 0)
+                    {
+                        errLabel1.Visible = true;
+                        errLabel1.Text = "Username has been Used!";
+                    }
+                    else
+                    {
+                        if (DropDownList1.SelectedItem.Value == "select")
+                        {
+                            errLabel3.Visible = true;
+                            errLabel3.Text = "Select your country!";
+                        }
+                        else
+                        {
+                            string q2 = "INSERT INTO MemberTable (Username,Password,FirstName,LastName,Country,DateJoined,LoginStatus)" +
+                                "VALUES (@Username,@Password,@FirstName,@LastName,@Country,@DateJoined,@LoginStatus)";
+                            SqlCommand cmd2 = new SqlCommand(q2, con);
+                            DateTime today = DateTime.Now.Date;
+
+                            cmd2.Parameters.AddWithValue("@FirstName", TextBox3.Text.Trim());
+                            cmd2.Parameters.AddWithValue("@LastName", TextBox4.Text.Trim());
+                            cmd2.Parameters.AddWithValue("@Country", DropDownList1.SelectedItem.Value);
+                            cmd2.Parameters.AddWithValue("@Username", TextBox1.Text.Trim());
+                            cmd2.Parameters.AddWithValue("@Password", TextBox2.Text.Trim());
+                            cmd2.Parameters.AddWithValue("@DateJoined", today.ToString("yyyy-MM-dd"));
+                            cmd2.Parameters.AddWithValue("@LoginStatus", "Pending");
+
+                            cmd2.ExecuteNonQuery();
+                            Response.Write("<script>alert('Sign Up Success!'); window.location='LoginPage.aspx';</script>");
+                        }
+                    }
+                    con.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    errLabel2.Visible = true;
+                    errLabel2.Text = "Registration Failed!" + ex.ToString(); // need to check
+                }
             }
+            
         }
     }
 }
